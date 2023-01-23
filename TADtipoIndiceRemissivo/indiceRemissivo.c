612708262bca557e0b526bf1fb3d56bcdab38a27
Nibi2003 <beatrizaguiar0202@gmail.com>
2023-01-22 22:10:46 -0400

    infoDic

--- TADtipoIndiceRemissivo/indiceRemissivo.c
+++ TADtipoIndiceRemissivo/indiceRemissivo.c
@@ -1,2 +1,14 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "indiceRemissivo.h"
+
+typedef struct pagOcorre{
+    int pagina;
+    int ocorrencias;
+}pagOcorre;
+
+struct InfoDic{
+    pagOcorre* vetDeOcorrencias;
+    int ocorrenciasTotal;
+    int paginasTotal;
+};
--- TADtipoIndiceRemissivo/indiceRemissivo.h
+++ TADtipoIndiceRemissivo/indiceRemissivo.h
@@ -2,12 +2,13 @@
 #define TIPOINDISSEREMISSIVO_H
 
 typedef struct tipoIndiceRemissivo tipoIndiceRemissivo;
-typedef struct InfoDic;
 
 tipoIndiceRemissivo * criarIndice(char*nomeArquivo, void*stopMundo);
 
 int buscarIndice(tipoIndiceRemissivo* nomeIndice, char* palavraDoArquivo);
 
 
+typedef struct InfoDic InfoDic;
+InfoDic* criarInfoDic();
 
 #endif
