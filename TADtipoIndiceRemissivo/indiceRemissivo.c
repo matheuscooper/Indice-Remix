671ca7905b6aa673f87a53e58bfb152a2e5d8922
Nibi2003 <beatrizaguiar0202@gmail.com>
2023-01-22 22:28:34 -0400

    tentei fazer algo aqui

--- TADtipoIndiceRemissivo/indiceRemissivo.c
+++ TADtipoIndiceRemissivo/indiceRemissivo.c
@@ -1,14 +1,18 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "indiceRemissivo.h"
+#include "../ListaEncadeada/ListaEncadeada.h"
 
 typedef struct pagOcorre{
     int pagina;
     int ocorrencias;
 }pagOcorre;
 
-struct InfoDic{
-    pagOcorre* vetDeOcorrencias;
+typedef struct InfoDic{
+    ListaE listaDePags;
     int ocorrenciasTotal;
     int paginasTotal;
-};
+}InfoDic;
+
+InfoDic* criarInfoDic(char* chave){
+
+}
