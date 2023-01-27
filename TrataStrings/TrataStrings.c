7808adfade6b8e2f7b1705bc8e75bb783118da36
oliveiracooper <oliveiracooper73@gmail.comm>
2023-01-27 17:44:23 -0300

    tratei strings

--- TrataStrings/TrataStrings.c
+++ TrataStrings/TrataStrings.c
@@ -1,11 +1,28 @@
 #include "stdio.h"
 
-char* LimpaString(char* string){
-    return 0;
+void LimpaString(char* string){
+    char * palavra = malloc(sizeof(char*));
+    int i,j = 0;
+    while ( string[i]!=0){
+       if (!(string[i] > 33 && string[i] < 43)){
+        palavra[j] = string[i];
+        j++;
+       } 
+       i++;
+    }
+     palavra[j]= 0;
+     strcpy(string,palavra); 
+     free(palavra);
 }
 
-char* LowerString(char* string){
-    return 0;
+void LowerString(char* string){
+    int i = 0;
+    while ( string[i]!=0){
+       if (!(string[i] > 65 && string[i] < 90)){
+        string[i]= string[i] + 32; 
+       } 
+       i++;
+}
 }
 
 double idefDF(long int ocorrencias, long int totalPalavras, int n_documentos, int total_documentos){
